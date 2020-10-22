47bffe5f2061274eb79b023367de02c3a8f4d84b
Andrew Shamah <42912128+amshamah419@users.noreply.github.com>
2020-10-22 11:18:11 +0300

    Prepare install only test playbooks needed

--- Tests/configure_and_test_integration_instances.py
+++ Tests/configure_and_test_integration_instances.py
@@ -1250,6 +1250,7 @@ def test_pack_metadata():
 def private_test_pack_zip():
     content_path = '/home/runner/work/content-private/content-private/content'
     target = '/home/runner/work/content-private/content-private/content/test_pack.zip'
+
     with zipfile.ZipFile(target, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         zip_file.writestr('test_pack/metadata.json', test_pack_metadata())
         for test_path, test in test_files(content_path):
@@ -1266,6 +1267,7 @@ def private_test_pack_zip():
                 else:
                     test_target = f'test_pack/TestPlaybooks/{test}'
                 zip_file.writestr(test_target, test_file.read())
+
 
 def test_pack_zip(build: Build):
     if build.is_private:
